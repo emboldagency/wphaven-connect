8e5cbca4f158a6d7caebe8b51a201cc145def038
embold-tyler <tyler@embold.com>
2025-03-13 07:12:28 -0700

    Display an environment indicator in the admin bar

--- wphaven.php
+++ wphaven.php
@@ -4,7 +4,7 @@
  * Plugin Name:        WPHaven Connect
  * Plugin URI:         https://embold.com
  * Description:        A plugin that provides functionality to connect to WPHaven.
- * Version:            0.8.0
+ * Version:            0.9.0
  * Author:             emBold
  * Author URI:         https://embold.com/
  * Primary Branch:     master
