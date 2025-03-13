a0a858a91bcbe54b20f5a6ad8aed41f374cee988
Xan <octoxan@users.noreply.github.com>
2025-03-13 14:16:24 -0400

    Disable error handler for anything not critical

--- src/ErrorHandler.php
+++ src/ErrorHandler.php
@@ -12,15 +12,14 @@ class ErrorHandler {
     }
 
     public function handle_error($errno, $errstr, $errfile, $errline) {
-        $error = compact('errno', 'errstr', 'errfile', 'errline');
-
-        $this->send_slack_notification($error);
-
-        // Optionally rethrow the error to be caught by shutdown function
-        if ($errno === E_ERROR) {
-            return false;
+        // Ignore non-fatal errors
+        if (!in_array($errno, [E_ERROR, E_CORE_ERROR, E_COMPILE_ERROR, E_USER_ERROR, E_RECOVERABLE_ERROR])) {
+            return true; // Ignore non-fatal errors
         }
-        return true;
+
+        $error = compact('errno', 'errstr', 'errfile', 'errline');
+        $this->send_slack_notification($error);
+        return false; // Ensure fatal errors are not suppressed
     }
 
     public function handle_exception($exception) {
@@ -37,7 +36,7 @@ class ErrorHandler {
     public function handle_shutdown() {
         $error = error_get_last();
 
-        if (isset($_SERVER['HTTP_HOST']) && $error) {
+        if (isset($_SERVER['HTTP_HOST']) && $error && in_array($error['type'], [E_ERROR, E_CORE_ERROR, E_COMPILE_ERROR, E_USER_ERROR, E_RECOVERABLE_ERROR])) {
             // This will catch syntax errors, fatal errors, etc.
             $this->send_slack_notification($error);
         }
@@ -65,20 +64,10 @@ class ErrorHandler {
     private function get_error_type($errno) {
         $types = [
             E_ERROR             => 'Fatal Error',
-            E_WARNING           => 'Warning',
-            E_PARSE             => 'Parse Error',
-            E_NOTICE            => 'Notice',
             E_CORE_ERROR        => 'Core Error',
-            E_CORE_WARNING      => 'Core Warning',
             E_COMPILE_ERROR     => 'Compile Error',
-            E_COMPILE_WARNING   => 'Compile Warning',
             E_USER_ERROR        => 'User Error',
-            E_USER_WARNING      => 'User Warning',
-            E_USER_NOTICE       => 'User Notice',
-            E_STRICT            => 'Strict Notice',
-            E_RECOVERABLE_ERROR => 'Recoverable Error',
-            E_DEPRECATED        => 'Deprecated',
-            E_USER_DEPRECATED   => 'User Deprecated',
+            E_RECOVERABLE_ERROR => 'Recoverable Error'
         ];
 
         return $types[$errno] ?? 'Unknown Error';
