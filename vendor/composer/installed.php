06e3ba1b1bb02295e988e121883bb220a219d571
tyler <tyler@embold.com>
2025-12-22 11:52:12 -0800

    :rewind: Re-include vendor and add export-ignore for dev files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wphaven/wphaven-connect',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'd6a13be63a00808d6737d993cf850ead861ae0e1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'wphaven/wphaven-connect' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'd6a13be63a00808d6737d993cf850ead861ae0e1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
